83323c762b1ea392e90be83d585f6531a7c48993
Ryan Campbell <rcampbell@arcaser.quest>
2024-02-08 20:37:25 -0500

    Flask Start
    ezpz

--- App/app.py
+++ App/app.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-from Mongo.MongoLoader import loader  # Make sure this import matches your file and class name
+from werkzeug.utils import secure_filename
+import os
+from Mongo.MongoLoader import loader  
+from Mongo.MongoReader import reader
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'App\img'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 # Use environment variables or another secure method to store and retrieve your credentials
-db_uri = "your_mongodb_connection_string"
+db_uri = "Look at discord"
 db_name = "hexThreads"
 
 # Initialize your loader instance correctly
-l = loader(db_uri, db_name)
+load = loader(db_uri, db_name)
+read = reader(db_uri, db_name)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/add_cloth_form')
-def add_cloth_form():
-    return render_template('add_cloth.html')
-
-@app.route('/add_cloth', methods=['GET', 'POST'])
-def add_cloth():
-    if request.method == 'POST':
-        data = request.form
-        l.insert_cloth(data['id'], data['store'], data['image'])
-        return redirect(url_for('add_cloth_form'))
-    else:
-        return render_template('add_cloth.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
