c2a78f1c2d868e1daef90c4c8061bf73eb6defaa
Ryan Campbell <campbellryan954@gmail.com>
2024-03-21 22:56:51 -0400

    /colorgrab /upload initial commit

--- App/app.py
+++ App/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from Controller.MongoLoader import loader  
-from Controller.MongoReader import reader
-from Controller.upload import Upload
-
+from flask import Flask , url_for, request, redirect , jsonify
+from colorthief import ColorThief
+from io import BytesIO
+import base64
+import db
 
 app = Flask(__name__)
 
@@ -14,35 +14,39 @@ db_name = "hexThreads"
 
 
 # Make a mongo main controller because this is starting to get messy
-# Initialize your loader instance correctly
-upload = Upload(db_uri, db_name)
-read = reader(db_uri, db_name)
+db = db(db_uri, db_name)
 
-# Define the home route
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return url_for('index.html')
 
-# Define the upload route
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'image' not in request.files:
-        print('No file part')
-        return redirect(url_for('home'))
-    file = request.files['image']
-    if file.filename == '':
-        print('No selected file')
-        return redirect(url_for('home'))
-    fileName = file.filename
-    valid = upload.allowed_file(fileName)
-    if file and valid:
+    if 'image_items' not in request.json:
+        return jsonify({"error": "Missing image_items in request"}), 400
 
-        if upload.upload_file(file):
-            print('File uploaded successfully')
-            # Redirect or process as needed
-        else:
-            print('File upload failed')
-    return redirect(url_for('home'))
+    for item in request.json['image_items']:
+        store_name = item.get('store')
+        image_data = item.get('binary')
+        
+        # Decode base64 image to binary if necessary
+        # Ensure you're receiving the binary data in base64 format from the client
+        image_binary = base64.b64decode(image_data)
+        
+        db.add_clothes_store(store_name, image_binary)
+
+    return jsonify({"message": "Upload successful"}), 200
+    
+#colorgrab route
+@app.route('/colorgrab/{store}/{rgb}', methods=['GET', 'POST'])
+def colorgrab(rgb,store):
+    if request.method == 'GET':
+        return "Please use POST method to send the RGB value"
+    else:
+        
+        results = db.get_colors(rgb, store)
+        return jsonify(results)
+    
 
 if __name__ == "__main__":
     app.run()
